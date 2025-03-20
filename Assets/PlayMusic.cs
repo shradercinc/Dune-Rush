0c0f600d8f69a59e54d179a3a163692744a3e75a
Nicole Conrad Shrader <82776089+shradercinc@users.noreply.github.com>
2025-03-20 18:41:56 -0400

    added basic music for menu

--- Assets/PlayMusic.cs
+++ Assets/PlayMusic.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlayMusic : MonoBehaviour
 {
+    [SerializeField] AK.Wwise.Event MusicIntro;
+    [SerializeField] AK.Wwise.Event MusicLoop;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MusicIntro.Post(gameObject, (uint)AkCallbackType.AK_EndOfEvent, CallBackHitFunction);
+    }
+
+    void CallBackHitFunction(object in_cookie, AkCallbackType callType, object in_info)
+    {
+        if (callType == AkCallbackType.AK_EndOfEvent)
+        {
+            MusicLoop.Post(gameObject);
+        }
     }
 
     // Update is called once per frame
